f71f5182bbbdb84a567ebcc9b80586de2748f451
huiguang.liang <huiguang.liang@nysnetech.com>
2016-09-28 14:04:37 +0800

    grunt_test

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+//包装函数
+module.exports = function(grunt) {
+
+	// 任务配置，所有插件的配置信息
+	grunt.initConfig({
+
+
+
+		//获取 package.json的信息
+		pkg: grunt.file.readJSON('package.json'),
+
+		// uglify插件的配置信息
+		uglify: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%=pkg.name%> - <%=pkg.version%>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'src/test.js',
+				dest: 'build/<%=pkg.name%> - <%=pkg.version%>.js.min.js'
+			}
+		},
+		// jshint的配置信息
+		jshint: {
+			build: ['Gruntfile.js', 'src/*.js'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
+		//watch的安装配置信息
+		watch: {
+			build: 
+			{
+			  files: ['src/*.js', 'src/*.css'],
+			  tasks: ['jshint', 'uglify'],
+			  options: {spawn: false}
+		    }
+		}
+	});
+    //告诉grunt我们将使用插件
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//告诉grunt当我们在终端输入grunt时需要做些什么(注意先后顺序)
+	grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
+};
